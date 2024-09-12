6f1e843a8b48ed0844216fb8f4ef6c0751c9be06
emimelano <emiliomelero05@gmail.com>
2024-09-12 19:02:49 +0200

    v2

---
+++ intro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EJERCICIOS INICIALES PHP</title>
+</head>
+<body>
+   <?php
+    $nombre = "Emilio Melero";
+    $anno = 2005;
+
+    echo "Me llamo $nombre y naci en $anno";
+   ?>
+</body>
+</html>
